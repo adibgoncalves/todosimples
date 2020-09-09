299848a1a4cebe03be36c63d545247802774bfb4
Adib Gonçalves <adib.fernandes@gmail.com>
2020-09-09 18:33:27 -0300

    Todo finalizado

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+var listElement = document.querySelector('#app ul');
+var inputElement = document.querySelector('#app input');
+var buttonElement = document.querySelector('#app button');
+
+var todos = JSON.parse(localStorage.getItem('list_todos')) || [];
+
+function renderTodos() {
+    listElement.innerHTML = '';
+
+    for (const todo of todos) {
+       var todoElement = document.createElement('li');
+       var todoText = document.createTextNode(todo);
+
+       var linkElement = document.createElement('a');
+
+       linkElement.setAttribute('href', '#');
+
+       var pos = todos.indexOf(todo); //Encontra a posicao do elemento no array de acordo com o conteudo
+       linkElement.setAttribute('onclick', 'deleteTodo(' + pos + ')');
+
+       var linkText = document.createTextNode('Excluir');
+
+       linkElement.appendChild(linkText);
+
+       todoElement.appendChild(todoText);
+       todoElement.appendChild(linkElement);
+
+       listElement.appendChild(todoElement);
+    }
+}
+
+renderTodos();
+
+function addTodo() {
+    var todoText = inputElement.value;
+
+    todos.push(todoText);
+    inputElement.value = '';
+    renderTodos();
+    saveToStorage();
+}
+
+buttonElement.onclick = addTodo;
+
+function deleteTodo(pos) {
+    todos.splice(pos, 1); //Vai na pos e remove 1 intem
+    renderTodos();
+    saveToStorage();
+}
+
+function saveToStorage() {
+    localStorage.setItem('list_todos', JSON.stringify(todos));
+}
